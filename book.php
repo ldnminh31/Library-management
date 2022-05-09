719b625c7fbcd21ebbc47b3f6f523ce054f0257a
ldnminh31 <ledoannhatminh31@gmail.com>
2022-05-09 15:42:07 +0700

    book

--- book.php
+++ book.php
@@ -4,37 +4,46 @@
     $data = get("SELECT * FROM sach");
     ?>
     <hr>
-    <!-- Xay dung frm tim kiem -->
+    
     <link rel="stylesheet" href="./css/style.css">
-    <div class="tim-kiem">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <p class="fs-4 text-center"><b>Tìm kiếm sách - Hãy nhập từ khóa</b></p>
-                </div>
-                <div class="col-12">
-                    <form action="" method="post">
-                        <input type="text" name="search" value="">
-                        <button type="submit" name="ok" value="search">Tìm kiếm</button>
-                        <button><a href="./filter.php">Lọc sách nâng cao</a></button>
-                    </form>
-                </div>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
+    <!-- Xay dung form tim kiem START-->
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <p class="fs-4 text-center"><b>Tìm kiếm sách - Hãy nhập từ khóa</b></p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <form class="text-center form-timkiem" action="" method="post">
+                    <input type="text" name="search" value="">
+                    <button class="btn btn-primary" type="submit" name="ok" value="search">Tìm kiếm</button>
+                    <a class="btn btn-primary" href="./filter.php">Lọc sách nâng cao</a>
+                </form>
             </div>
         </div>
     </div>
+    <!-- Xay dung form tim kiem END -->
     <hr>
+
+
+
     <?php
-    // tim kiem
+    // PHP TIM KIEM END 
     if (isset($_REQUEST['ok'])) {
         // Gán hàm addslashes để chống sql injection
         $search = addslashes($_POST['search']);
 
-        // Nếu $search rỗng thì báo lỗi, tức là người dùng chưa nhập liệu mà đã nhấn submit.
+        // Nếu người dùng chưa nhập liệu mà đã nhấn submit.
+ 
         if (empty($search)) {
             echo "<p class='fs-5 text-center'>Nhập từ khóa!</p>";
-        } else {
-
+        }elseif ($search == ' '){
+            echo "<p class='fs-5 text-center'>Nhập từ khóa!</p>";
+        } 
+        else {
 
             // Kết nối sql
             $con =  mysqli_connect("localhost", "root", "", "qltv");
@@ -64,12 +73,12 @@
                 echo '<div class="container-fluid result">';
                 while ($row = mysqli_fetch_assoc($sql)) {
                     echo '<div class="row">';
-                    echo "<div class='col-2'>{$row['tensach']}</div>";
-                    echo "<div class='col-2'>{$row['tentacgia']}</div>";
-                    echo "<div class='col-2'>{$row['theloai']}</div>";
-                    echo "<div class='col-4'>{$row['mota']}</div>";
-                    echo "<div class='col-1'>{$row['vitri']}</div>";
-                    echo "<div class='col-1'>{$row['soluong']}</div>";
+                    echo "<div class='col-2 text-center'>{$row['tensach']}</div>";
+                    echo "<div class='col-2 text-center'>{$row['tentacgia']}</div>";
+                    echo "<div class='col-2 text-center'>{$row['theloai']}</div>";
+                    echo "<div class='col-4 text-center'>{$row['mota']}</div>";
+                    echo "<div class='col-1 text-center'>{$row['vitri']}</div>";
+                    echo "<div class='col-1 text-center'>{$row['soluong']}</div>";
                     echo '</div>';
                     echo '<hr>';
                 }
@@ -80,14 +89,14 @@
         }
     }
     ?>
-
+    <!-- PHP TIM KIEM END -->
 
     <!-- Load db -->
     <p class="fs-4 text-center"><b>Tất cả sách trong thư viện</b></p>
     <button class="btn btn-primary mx-auto d-block" data-bs-toggle="modal" data-bs-target="#exampleModal">Thêm sách</button>
     <table class="table ">
         <tr>
-            <th class="text-center">STT</th>
+            <th class="text-center">Mã sách</th>
             <th class="text-center">Tên sách</th>
             <th class="text-center">Tác giả</th>
             <th class="text-center">Thể loại</th>
